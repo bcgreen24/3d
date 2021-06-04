0479e07b1bf786f98f755fb3e61e82ab44c187cc
Bryan C Green <bcgreen@gmail.com>
2021-06-04 17:26:52 -0500

    only the ball

--- index.js
+++ index.js
@@ -3,24 +3,19 @@ function init(){
     var scene = new THREE.Scene();
     scene.fog = new THREE.FogExp2(0x444444, 0.18);
     scene.physicallyCorrectLights = true;
-    var box = getBox(1,1,1);   
-    var plane = getPlane(10,10);
-    var sphere = getSphere(0.5);
+    
+    var sphere = getSphere(1);
     sphere.name = 'ball';
     var pointLight = getPointLight(2);
 
-    pointLight.position.y = 4.5;
-    plane.name = 'plane1';
-    plane.rotation.x = Math.PI/2;
-    box.position.y = box.geometry.parameters.height/2;
-    sphere.position.y = 1;
-    sphere.position.z = -0.5;
-    sphere.position.x = 1.5;
-    box.name = 'box1';
-    scene.add(sphere);
-    
-    scene.add(box);
-    scene.add(plane);
+    pointLight.position.y = 5.5;
+
+
+    sphere.position.y = 0;
+    sphere.position.z = 0;
+    sphere.position.x = 0;
+      scene.add(sphere);
+
     scene.add(pointLight);
 
     var camera = new THREE.PerspectiveCamera(
@@ -29,7 +24,7 @@ function init(){
         1,
         1000
     );
-    camera.position.z = 5;
+    camera.position.z = 0;
     camera.position.y = 3;
     camera.position.x = 2;
     camera.lookAt(new THREE.Vector3(0,0,0,0));
@@ -69,12 +64,12 @@ function getSphere(r){
 
     var material = new THREE.MeshStandardMaterial({
         color: 0xcccccc,
-        roughness: 0.7
+        roughness: 0.9
     });
 
     material.map = loader.load('baseball.jpg');
     material.bumpMap = loader.load('baseball.jpg');
-    material.bumpScale = 0.01;
+    material.bumpScale = 0.002;
     
     var mesh = new THREE.Mesh(
         geometry,
@@ -101,7 +96,6 @@ function getPlane(w,l){
 }
 
 function update(renderer, scene, camera){
-    scene.getObjectByName('box1').rotation.y -= 0.005;
     scene.getObjectByName('ball').rotation.y += 0.005;
     scene.getObjectByName('ball').rotation.x += 0.005;
     renderer.render(
